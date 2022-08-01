a3870a6539a785ffc83ee6f4eabc8e866660ee06
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-08-01 15:07:52 +1000

    reat test, editing

---
+++ client/src/components/MarketItemEdit.js
@@ -0,0 +1,115 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+const MarketItemEdit = (props) => {
+  const initialState = {
+    name: "",
+    categories: "",
+    image: "",
+    description: "",
+    price: 0,
+    deliverable: false,
+    location: "",
+  };
+
+  const [fields, setFields] = useState(initialState);
+  const { itemID } = useParams();
+  const editedItem = props.marketplace.find((item) => {
+    return itemID === item._id;
+  });
+
+  const handleChange = (event) => {
+    const { name, value, checked } = event.target;
+    setFields({
+      ...fields,
+      [name]: event.target.type === "checkbox" ? checked : value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    const index = props.marketplace.findIndex(editedItem);
+    event.preventDefault();
+    // props.setMarketplace(fields);
+    props.handleEdit(fields, itemID, index);
+    console.log(fields, itemID, index);
+    // setFields(initialState);
+  };
+  return (
+    <div>
+      <h1>Editing {editedItem.name}</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name: </label>
+          <input
+            name="name"
+            type="text"
+            value={fields.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Categories: </label>
+          <input
+            name="categories"
+            type="text"
+            onChange={handleChange}
+            value={fields.categories}
+          />
+        </div>
+        <div>
+          <label>Image: </label>
+          <input
+            name="image"
+            type="text"
+            onChange={handleChange}
+            value={fields.image}
+          />
+        </div>
+        <div>
+          <label>Description: </label>
+          <textarea
+            name="description"
+            type="text"
+            onChange={handleChange}
+            value={fields.description}
+          />
+        </div>
+        <div>
+          <label>Deliverable: </label>
+          {fields.deliverable ? (
+            <input
+              name="deliverable"
+              type="checkbox"
+              onClick={handleChange}
+              checked
+            />
+          ) : (
+            <input name="deliverable" type="checkbox" onClick={handleChange} />
+          )}
+        </div>
+        <div>
+          <label>Price: </label>
+          <input
+            name="price"
+            type="number"
+            onChange={handleChange}
+            value={fields.price}
+          />
+        </div>
+        <div>
+          <label>Location: </label>
+          <input
+            name="location"
+            type="text"
+            onChange={handleChange}
+            value={fields.location}
+          />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default MarketItemEdit;
---
+++ client/src/components/Marketplace.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Marketitem = (props) => {
+  return (
+    <div key={props.item._id}>
+      <h1>{props.item.name}</h1>
+      <img src={props.item.image} />
+      <p>{props.item.description}</p>
+      <Link to={`/marketplace/${props.item._id}`}>Show</Link>
+    </div>
+  );
+};
+
+const Marketplace = (props) => {
+  const MarketplaceList = props.marketplace.map((item) => {
+    return <Marketitem key={item._id} item={item} />;
+  });
+  return <div>{MarketplaceList}</div>;
+};
+
+export default Marketplace;
