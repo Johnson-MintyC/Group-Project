a5e0456633697ebbb0475a4710f9783d6f5e98de
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-08-01 10:54:13 +1000

    SChema

---
+++ api/models/marketplace.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const marketplaceSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  categories: { type: String },
+  price: { type: Number, required: true, min: 0 },
+  location: { type: String, required: true },
+  image: { type: String },
+  description: { type: String },
+  deliverable: { type: Boolean },
+});
+
+const Marketplace = mongoose.model("Marketplace", marketplaceSchema);
+
+module.exports = Marketplace;
