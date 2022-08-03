c94cc1cb74c96f3dacb26f5fb56079886725009d
Johnson Cho <johnsoncho@Roys-MBP.gateway>
2022-08-04 00:06:30 +1000

    removed dupe merge files

--- api/models/marketplace.js
+++ api/models/marketplace.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 
-const marketplaceSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  categories: { type: String },
-  price: { type: Number, required: true, min: 0 },
-  location: { type: String, required: true },
-  image: { type: String },
-  description: { type: String },
-  deliverable: { type: Boolean },
-},{timestamps:true});
+const marketplaceSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    categories: { type: String },
+    price: { type: Number, required: true, min: 0 },
+    location: { type: String, required: true },
+    image: { type: String },
+    description: { type: String },
+    deliverable: { type: Boolean },
+    postowner: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Marketplace = mongoose.model("Marketplace", marketplaceSchema);
 
